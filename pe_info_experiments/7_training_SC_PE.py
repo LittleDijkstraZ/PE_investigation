f48ea15b32153b5c3cd98305378d0d28238189ee
LittleDijkstraZ <littedijkstra_wsl>
2023-12-03 02:25:08 -0500

    start training for all

--- pe_info_experiments/7_training_SC_PE.py
+++ pe_info_experiments/7_training_SC_PE.py
@@ -6,8 +6,7 @@ from functools import partial
 
 
 def run_training(out_name,
-    args,
-    ):
+    args,):
     layerwise_pe, use_residual = args
     pe_type='original'
     params = {
@@ -18,6 +17,8 @@ def run_training(out_name,
         'pe_type': pe_type,  # or 'sin'
         'learning_rate': 0.00055221,
         'warmup_iters': 422,
+
+
         'use_residual': use_residual,
         'layerwise_pe': layerwise_pe,
 
@@ -70,17 +71,22 @@ if __name__ == "__main__":
     # layerwise_pe_list = [False,]*6
 
     # no SC[i] SC[i+1] yes lwp[i], lwp[i+1]
-    # use_residual_list = [j for j in range(6) if j not in [i, i+1] for i in range(5)]
+    # use_residual_list = [[j for j in range(6) if j not in [i, i+1]] for i in range(5)]
     # layerwise_pe_list = [[i, i+1] for i in range(5)]
 
     # control
+    # use_residual_list = [[j for j in range(6) if j not in [i, i+1]] for i in range(5)]
+    # layerwise_pe_list = [False,]*5
+
+    # no SC[i] SC[i+1] yes lwp=True
     use_residual_list = [[j for j in range(6) if j not in [i, i+1]] for i in range(5)]
-    layerwise_pe_list = [False,]*5
+    layerwise_pe_list = [True for i in range(5)]
+    
 
     # do a multi-processing, using 2 processes at a time
     from multiprocessing import Pool
     from functools import partial
     pool = Pool(1)
     func = partial(run_training, out_name)
-    pool.map(func, list(zip(layerwise_pe_list, use_residual_list)))
+    pool.map(func, list(zip(layerwise_pe_list, use_residual_list,)))
     pool.close()
